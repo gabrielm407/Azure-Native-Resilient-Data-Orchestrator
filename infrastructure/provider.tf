cefb8cf98813e0764b78c883a72cf12365599876
gabrielm407 <moffettgabriel@gmail.com>
2026-02-04 17:49:12 -0800

    Terraform pipeline

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -6,16 +6,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0"
+      version = "~> 3.0"
     }
   }
+
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "Default Project"
+    organization = "Azure-Native-Resilient-Data-Orchestrator"
 
     workspaces {
       name = "Azure-Native-Resilient-Data-Orchestrator"
@@ -25,8 +26,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  tenant_id       = var.ARM_TENANT_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
 }
